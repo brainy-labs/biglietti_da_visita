251c698e61c216cf4c93171c1ed90d86484c0f33
Vizzini Rosaria <r.vizzini@unisalute.it>
2022-05-06 11:16:21 +0200

    allineamento da revisionare

--- lib/RegistrationPage.dart
+++ lib/RegistrationPage.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 
 class RegistrationPage extends StatefulWidget {
@@ -12,6 +14,15 @@ class _RegistrationPageState extends State<RegistrationPage> {
   /// Global key that uniquely identifies the Form widget  and allows validation of the form.
   final _formKey = GlobalKey<FormState>();
   List<String> registrationFields = ['nome', 'cognome', 'mail', 'password', 'conferma Password'];
+  final HashMap<String, TextEditingController> _controllers = HashMap();
+
+  @override
+  void initState() {
+    super.initState();
+    addController();
+    print("init state");
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +33,7 @@ class _RegistrationPageState extends State<RegistrationPage> {
         child: ConstrainedBox(
           constraints: BoxConstraints(),
           child: Container(
-            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
             child: Form(
               key: _formKey,
               child: Center(
@@ -31,19 +42,20 @@ class _RegistrationPageState extends State<RegistrationPage> {
                   children: [
                     for (var item in registrationFields) 
                       TextFormField(
+                        controller: _controllers[item],
                         decoration: InputDecoration(labelText: item),
-                        autofocus: true,
-                        validator: (value) => value == null || value.isEmpty ? 'Il campo $item contiene uno o più errori' : null,
+                        autofocus: false,
+                        validator: (value) => validate(item, value!),
                       ),
                     Container(
-                      margin: EdgeInsets.all(20.0),
+                      margin: const EdgeInsets.all(20.0),
                       child: ElevatedButton(
                         onPressed: doRegister,
-                        child: Text('Submit'),
+                        child: const Text('Submit'),
                         style: ElevatedButton.styleFrom(
-                          primary: Color.fromARGB(255, 1, 29, 52),
-                          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                          textStyle: TextStyle(
+                          primary: const Color.fromARGB(255, 1, 29, 52),
+                          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                          textStyle: const TextStyle(
                               color: Colors.white,
                               fontSize: 15
                           ),
@@ -59,12 +71,40 @@ class _RegistrationPageState extends State<RegistrationPage> {
     );
   }
   doRegister () {
-    _formKey.currentState!.validate();
-    // if (_formKey.currentState!.validate()) {
-    //   ScaffoldMessenger.of(context).showSnackBar(
-    //     const SnackBar(content: Text('Processing Data')),
-    //   );
-    // }
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Processing Data')),
+      );
+    }
   }
 
+  validate(String field, String value){
+    String pattern = "";
+
+    if(value.isEmpty) {
+      return 'Il campo $field non è compilato';
+    } else if(field == 'mail') {
+      pattern = r"^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$";
+    }  else if(field =='password' || field == 'conferma Password') {
+      pattern = r"^.{8,}$";
+    } else if(field =='nome' || field == 'cognome') {
+      pattern = r"^[A-Za-zèùàòé][a-zA-Z\'èùàòéì ]*$";
+    }
+
+    var regex = RegExp(pattern);
+    if(!regex.hasMatch(value)) {
+      return 'Il campo $field contiene uno o più errori';
+    }
+  }
+
+  addController(){
+    for (var e in registrationFields) {
+      _controllers[e] = TextEditingController();
+      _controllers[e]!.addListener(() { doSomething(e);});
+    }
+  }
+
+  doSomething(e){
+    print(_controllers[e]?.text??'No Value');
+  }
 }
